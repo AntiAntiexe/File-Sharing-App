e5397d11b42e9bd5affc041da9881d2451ce24ed
Anton <mrbricken@gmail.com>
2024-05-12 15:53:17 +1000

    first

---
+++ main].py
@@ -0,0 +1,32 @@
+import customtkinter
+from customtkinter import*
+
+
+app = CTk()
+app.title("Text Editor")
+app.geometry("2560x1600")
+set_appearance_mode("dark")
+
+
+#frames
+frame = CTkFrame(master=app,fg_color="#FFFFFF", border_color='#FFFFFE', border_width=2, width=1000, height=200)
+frame.grid(row=0, column=0, padx=10, pady=10)
+
+#fonts
+my_font = customtkinter.CTkFont(family="Poppins", size=75, weight="bold", )
+
+my_font2 = customtkinter.CTkFont(family="sans serif", size=25, weight="normal",)
+
+#labels
+label = CTkLabel(master=frame, text="Menu",font=(my_font,70,'bold'), text_color='black')
+#label.grid(row=0, column=0, padx=10, pady=10)
+#label.place(relx=0.5,rely=0.05, anchor=CENTER)
+
+
+
+#textbox
+
+maintext = CTkTextbox(master=app, width=1500, height=1001 )
+#maintext.grid(row=0, column=2, padx=10, pady=10)
+
+app.mainloop()
