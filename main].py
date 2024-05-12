7af5be1e6403828da1e3a0db35240aea7e1b3d4e
Anton <mrbricken@gmail.com>
2024-05-12 19:23:20 +1000

    first

--- main].py
+++ main].py
@@ -1,4 +1,5 @@
 import customtkinter
+from test_import import start
 from customtkinter import*
 
 
@@ -6,10 +7,11 @@ app = CTk()
 app.title("Text Editor")
 app.geometry("2560x1600")
 set_appearance_mode("dark")
+#functions
 
 
 #frames
-frame = CTkFrame(master=app,fg_color="#FFFFFF", border_color='#FFFFFE', border_width=2, width=1000, height=200)
+frame = CTkFrame(master=app,fg_color="#FFFFFF", border_color='#FFFFFE', border_width=2, width=1900, height=200)
 frame.grid(row=0, column=0, padx=10, pady=10)
 
 #fonts
@@ -18,15 +20,13 @@ my_font = customtkinter.CTkFont(family="Poppins", size=75, weight="bold", )
 my_font2 = customtkinter.CTkFont(family="sans serif", size=25, weight="normal",)
 
 #labels
-label = CTkLabel(master=frame, text="Menu",font=(my_font,70,'bold'), text_color='black')
+label = CTkLabel(master=frame, text="Easy file sharing",font=(my_font,70,'bold'), text_color='black')
 #label.grid(row=0, column=0, padx=10, pady=10)
-#label.place(relx=0.5,rely=0.05, anchor=CENTER)
+label.place(relx=0.5,rely=0.4, anchor=CENTER)
 
 label = CTkLabel(master=frame, text="hello did this work", font=(my_font))
 
-#textbox
-
-maintext = CTkTextbox(master=app, width=1500, height=1001 )
-#maintext.grid(row=0, column=2, padx=10, pady=10)
-
+#buttons
+button = CTkButton(master=app,command=lambda : start('192.168.1.111', 8000), text="Launch Server")
+button.place(relx=0.5,rely=0.5,anchor=CENTER)
 app.mainloop()
